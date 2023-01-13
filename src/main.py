704f9fe92a1e4385d7caffa5f6822e2bb60b5455
djjd13 <jdeggert98@gmail.com>
2023-01-12 22:02:25 -0600

    Added stat generation for the 2002 season, and refactored the code in main to use the __name__ == "__main__" format. Also added the ability to change between the 2 'modes'

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import mysql.connector
 import re
 import utils
 
+
 # ENDPOINT = "baseballstats-mysql-aws.co5amxrfxw6w.us-east-1.rds.amazonaws.com"
 # PORT = 3306
 # USER = "mike_ohtani"
@@ -72,67 +73,134 @@ def main():
         'WAS': {'city': 'Washington', 'name': 'Nationals'}
     }
 
+    event_type_dict = {
+        0: "Unkown Event",
+        1: "No event",
+        2: "Generic Out",
+        3: "Strikeout",
+        4: "Stolen Base",
+        5: "Defensive Indifference",
+        6: "Caught Stealing",
+        7: "Pickoff error",
+        8: "Pickoff",
+        9: "Wild Pitch",
+        10: "Passed ball",
+        11: "Balk",
+        12: "Other advance",
+        13: "Foul Error",
+        14: "Walk",
+        15: "Intentional Walk",
+        16: "Hit by pitch",
+        17: "Interference",
+        18: "Error",
+        19: "Fielder's Choice",
+        20: "Single",
+        21: "Double",
+        22: "Triple",
+        23: "Home Run",
+        24: "Missing Play"
+    }
+
     print(" Welcome to JD's BaseBall Stats Generator and Game Log Finder!")
     print(" Type --help for more information about formatting and options.")
 
     while True:
-        year = input(" Enter the year you want to find, or a specific game date/range of dates: ")
-        if not year.isnumeric():
-            if date_check.match(year):
-                year_begin = year[0: year.index(" ")]
-                year_end = year[year.index(" ") + 3:]
-            elif year == "--help":
-                print(" Enter a single year in the format YYYY (ex: 2013)")
-                print(" Enter a range of dates in the format 'YYYY-MM-DD - YYYY-MM-DD' (ex: 2013-03-31 - 2013-06-31)")
-                print(" Enter an abbreviation for the team you wish to find. Type '--teams' to see a list of acceptable"
-                      " abbreviations")
-                continue
-            elif year == "--teams":
-                for team in team_dict:
-                    print(" " + team + ": " + team_dict[team]['city'] + " " + team_dict[team]['name'])
-                continue
-            else:
-                print("Please enter a valid year or range of years.")
-                continue
-        else:
-            year_begin = year + "-01-01"
-            year_end = year + "-12-31"
-
-        team = input(" Enter the home team name: ")
-        team_city = team_dict[team]['city']
-        team_name = team_dict[team]['name']
-
-        amount_of_games = int(input(" Enter the amount of games you want to find: "))
+        stat_choice = input("Select which mode you want: Game Log or 2002 Season > ")
         print()
 
-        query_game = f'SELECT visitingTeam, visitingScore, homeTeam, homeScore, visitingLineScore, homeLineScore, ' \
-                     f'vAtBats, vHits, vDoubles, vTriples, vHomeRuns, vRBI, ' \
-                     f'hAtBats, hHits, hDoubles, hTriples, hHomeRuns, hRBI ' \
-                     f'FROM game_logs ' \
-                     f'WHERE (homeTeam = \'{team}\' OR visitingTeam = \'{team}\') AND ' \
-                     f'(gameDate >= \'{year_begin}\' AND gameDate <= \'{year_end}\') ORDER BY gameDate ' \
-                     f'LIMIT {amount_of_games}'
+        if stat_choice == "Game Log":
+            year = input(" Enter the year you want to find, or a specific game date/range of dates: ")
+            if not year.isnumeric():
+                if date_check.match(year):
+                    year_begin = year[0: year.index(" ")]
+                    year_end = year[year.index(" ") + 3:]
+                elif year == "--help":
+                    print(" Enter a single year in the format YYYY (ex: 2013)")
+                    print(" Enter a range of dates in the format 'YYYY-MM-DD - YYYY-MM-DD' (ex: 2013-03-31 - 2013-06-31)")
+                    print(" Enter an abbreviation for the team you wish to find. Type '--teams' to see a list of acceptable"
+                          " abbreviations")
+                    continue
+                elif year == "--teams":
+                    for team in team_dict:
+                        print(" " + team + ": " + team_dict[team]['city'] + " " + team_dict[team]['name'])
+                    continue
+                else:
+                    print("Please enter a valid year or range of years.")
+                    continue
+            else:
+                year_begin = year + "-01-01"
+                year_end = year + "-12-31"
 
-        baseball_cursor.execute(query_game)
+            team = input(" Enter the team abbreviation: ")
+            team_city = team_dict[team]['city']
+            team_name = team_dict[team]['name']
 
-        result = baseball_cursor.fetchall()
+            print(" " + team_city + " " + team_name)
 
-        team_wins, \
-            total_runs_scored, \
-            total_runs_allowed, \
-            total_at_bats, \
-            total_hits = utils.box_score_generator(result, team)
+            amount_of_games = int(input(" Enter the amount of games you want to find: "))
+            print()
 
-        utils.total_stat_printer(
-            amount_of_games,
-            team_wins,
-            total_runs_scored,
-            total_runs_allowed,
-            total_at_bats,
-            total_hits
-        )
+            query_game = f'SELECT visitingTeam, visitingScore, homeTeam, homeScore, visitingLineScore, homeLineScore, ' \
+                         f'vAtBats, vHits, vDoubles, vTriples, vHomeRuns, vRBI, ' \
+                         f'hAtBats, hHits, hDoubles, hTriples, hHomeRuns, hRBI ' \
+                         f'FROM game_logs ' \
+                         f'WHERE (homeTeam = \'{team}\' OR visitingTeam = \'{team}\') AND ' \
+                         f'(gameDate >= \'{year_begin}\' AND gameDate <= \'{year_end}\') ORDER BY gameDate ' \
+                         f'LIMIT {amount_of_games}'
+
+            query_season = f'SELECT visitingTeam, visitingScore, homeTeam, homeScore, visitingLineScore, homeLineScore, ' \
+                           f'vAtBats, vHits, vDoubles, vTriples, vHomeRuns, vRBI, ' \
+                           f'hAtBats, hHits, hDoubles, hTriples, hHomeRuns, hRBI ' \
+                           f'FROM game_logs ' \
+                           f'WHERE (homeTeam = \'{team}\' OR visitingTeam = \'{team}\') AND ' \
+                           f'(gameDate >= \'{year_begin}\' AND gameDate <= \'{year_end}\') ORDER BY gameDate '
+
+            baseball_cursor.execute(query_game)
+
+            result = baseball_cursor.fetchall()
+
+            team_wins, \
+                total_runs_scored, \
+                total_runs_allowed, \
+                total_at_bats, \
+                total_hits = utils.box_score_generator(result, team)
+
+            utils.total_stat_printer(
+                amount_of_games,
+                team_wins,
+                total_runs_scored,
+                total_runs_allowed,
+                total_at_bats,
+                total_hits
+            )
+
+        elif stat_choice == "2002 Season":
+            player_code = input("Enter the player you wish to find: ")
+            print()
+
+            query_2002_season_batters = f'SELECT game_id, pitching_sequence, res_batter, event_text, event_type, ' \
+                                        f'rbi_on_play, ab_flag, sf_flag ' \
+                                        f'FROM regular_season_2002 ' \
+                                        f'WHERE res_batter = \'{ player_code }\' ' \
+
+            query_player_name = f'SELECT last_name, first_name, team, field_position ' \
+                                f'FROM roster_2002 ' \
+                                f'WHERE player_id = \'{ player_code }\' '
+
+            baseball_cursor.execute(query_2002_season_batters)
+            result = baseball_cursor.fetchall()
+
+            baseball_cursor.execute(query_player_name)
+            player_name = baseball_cursor.fetchall()
+
+            utils.batter_stat_generator(result, player_name)
+
+        else:
+            print("Please type a valid mode to set")
+            continue
 
         break
 
 
-main()
+if __name__ == '__main__':
+    main()
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+import math
+
+
 def box_score_generator(result, team):
     total_wins = 0
     tot_runs_scored = 0
@@ -67,3 +70,151 @@ def total_stat_printer(total_games, wins, runs_scored, runs_allowed, at_bats, hi
         round(runs_allowed / total_games, 2)))
     print()
     print("Batting average over " + str(total_games) + " : " + str(round(hits / at_bats, 3)))
+
+
+def batter_stat_generator(result, player):
+    walks = 0
+    strikeouts = 0
+    hits = 0
+    hbp = 0
+    errors = 0
+    sf_flies = 0
+    singles = 0
+    doubles = 0
+    triples = 0
+    homeruns = 0
+
+    # Total amount of times the player was at the plate. It is NOT the same as at bats
+    plate_appearances = 0
+
+    # Total at bats. These are plate appearances that do NOT end in:
+    # Walk, HBP, Sac Fly/Bunt(Hit), Interference, or replaced
+    at_bats = 0
+
+    for play in result:
+        # game_id = play[0]
+        # pitching_seq = play[1]
+        # batter = play[2]
+        # event_text = play[3]
+        event_type = play[4]
+        # rbi_on_play = play[5]
+        ab_flag = play[6]
+        sf_flag = play[7]
+
+        plate_appearances += 1
+
+        # Generic Out
+        if event_type == 2:
+            if ab_flag == 'T':
+                at_bats += 1
+            if sf_flag == 'T':
+                sf_flies += 1
+        # Strikeout
+        elif event_type == 3:
+            at_bats += 1
+            strikeouts += 1
+        # Walk
+        elif event_type == 14 or event_type == 15:
+            walks += 1
+        # HBP
+        elif event_type == 16:
+            hbp += 1
+        # Error
+        elif event_type == 18:
+            at_bats += 1
+            errors += 1
+        # Fielder's Choice
+        elif event_type == 19:
+            at_bats += 1
+        # Single
+        elif event_type == 20:
+            at_bats += 1
+            hits += 1
+            singles += 1
+        # Double
+        elif event_type == 21:
+            at_bats += 1
+            hits += 1
+            doubles += 1
+        # Triple
+        elif event_type == 22:
+            at_bats += 1
+            hits += 1
+            triples += 1
+        # Home Run
+        elif event_type == 23:
+            at_bats += 1
+            hits += 1
+            homeruns += 1
+        else:
+            plate_appearances += 0
+
+    # Total number of bases the player achieved in hits (1 for S, 2 for D, 3 for T, 4 for HR)
+    total_bases = singles + (2 * doubles) + (3 * triples) + (4 * homeruns)
+
+    # Shows how often a player's At bats ended in a hit (Hits / At Bats)
+    batting_avg = round(hits / at_bats, 3)
+
+    # Shows how often a player's PA ended in a walk (Walks / Plate Appearances)
+    bb_avg = round(walks / plate_appearances, 3)
+
+    # Shows how often a player ended up on base, no matter how it was done
+    # (Hits + Walks + HBP / At Bats + Walks + HBP + Sac Fly)
+    on_base_per = round((hits + walks + hbp) / (at_bats + walks + hbp + sf_flies), 3)
+
+    # Shows batting average with weight for the number of bases the player gets in a hit
+    # (Singles + 2*Doubles + 3*Triples + 4*HRs) / At Bats
+    slugging_per = round(total_bases / at_bats, 3)
+
+    # Percentage of times a player struck out (Strikeouts / At bats)
+    strikeout_ratio = round(strikeouts / at_bats, 3)
+
+    # On base + slugging. Good overall way to see a hitter's general performance
+    on_base_plus_slugging = round(on_base_per + slugging_per, 3)
+
+    batter_stat_printer(
+        player,
+        plate_appearances,
+        at_bats,
+        total_bases,
+        batting_avg,
+        bb_avg,
+        on_base_per,
+        slugging_per,
+        strikeout_ratio,
+        on_base_plus_slugging
+    )
+
+
+def batter_stat_printer(player, pa, ab, tb, ba, bb_avg, obp, slg, str_r, ops):
+    player = player[0]
+    last_name = player[0]
+    first_name = player[1]
+    print(f'2002 Stats for { first_name } { last_name }: ')
+    print()
+    print("Plate Appearances: " + str(pa))
+    print("At Bats: " + str(ab))
+    print("Total bases: " + str(tb))
+    print("Batting Average: " + str(ba))
+    print("Base on Balls Average: " + str(bb_avg))
+    print("On Base Percentage: " + str(obp))
+    print("Slugging Percentage: " + str(slg))
+    print("Strikeout ratio: " + str(str_r))
+    print("On Base Plus Slugging: " + str(ops))
+    print()
+    print()
+
+
+def round_decimals_down(number: float, decimals: int = 2):
+    """
+    Returns a value rounded down to a specific number of decimal places.
+    """
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more")
+    elif decimals == 0:
+        return math.floor(number)
+
+    factor = 10 ** decimals
+    return math.floor(number * factor) / factor
